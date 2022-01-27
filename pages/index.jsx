82f4a68dc549ba283988ea91d9e9511ec816437f
Jefferson <jeffersonbraster@gmail.com>
2022-01-26 22:21:21 -0300

    iniciando o componente porta

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Gift from "../components/Gift";
+import Door from "../components/Door";
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Gift />
+      <Door />
     </div>
   );
 }
